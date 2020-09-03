4c39c5ee2e0443ef697ecac4a04532cfb443c83f
Emily Jane McTavish <ejmctavish@gmail.com>
2020-09-03 11:36:22 -0700

    docs

--- chronosynth/chronogram.py
+++ chronosynth/chronogram.py
@@ -1,9 +1,11 @@
-
+"""Functions for working with chronograms from phylesystem"""
 #!/usr/bin/env python3
 from opentree import OT
 
 
 def find_chronograms():
+    """Gets all study_ids from phylesystem where 'ot:branchLengthMode' == 'ot:time'
+    Returns a list of study_ids"""
     output = OT.find_trees(search_property="ot:branchLengthMode", value="ot:time")
     chronograms = set()
     for study in output.response_dict["matched_studies"]:
