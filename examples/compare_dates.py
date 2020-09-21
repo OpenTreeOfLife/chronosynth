224ba625916231688df79319a0c44eff1b9cfefc
Luna Sare <sanchez.reyes.luna@gmail.com>
2020-09-21 16:40:36 -0700

    debugging map conflict tools

--- examples/compare_dates.py
+++ examples/compare_dates.py
@@ -1,4 +1,4 @@
-from chronosynth.chronogram import map_conflict_ages
+from chronosynth import chronogram
 
 
 
@@ -9,5 +9,26 @@ res_ot1056 = map_conflict_ages('ot_1056@Tr66755')
 
 for synth_node in res_ot1056['supported_nodes']:
     if synth_node in res_ot1000['supported_nodes']:
-        print((res_ot1056['supported_nodes'][synth_node]['age'], res_ot1000['supported_nodes'][synth_node]['age'])) 
- 
+        print((res_ot1056['supported_nodes'][synth_node]['age'], res_ot1000['supported_nodes'][synth_node]['age']))
+
+errors = []
+no_conflict = []
+studies = chronogram.find()
+
+for tag in studies:
+    try:
+        res = map_conflict_ages(tag)
+    except ValueError:
+        errors.append(tag)
+        print("study", tag, "threw an ERROR on map_conflict ages")
+    if res==None:
+        print("study", tag, "has no conflict data")
+        no_conflict.append(tag)
+    else:
+        print("study", tag, "has", len(res["supported_nodes"]), "supported_nodes")
+
+
+studies.index('ot_1876@tree1')
+studies[280]
+source_id=studies[69]
+map_conflict_ages('ot_1876@tree1')
