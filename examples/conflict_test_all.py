80698f585c8699a8426ba3dac62123911f8ea4b9
Emily Jane McTavish <ejmctavish@gmail.com>
2020-10-01 13:09:29 -0700

    update

--- examples/conflict_test_all.py
+++ examples/conflict_test_all.py
@@ -2,11 +2,13 @@
 
 from opentree import OT
 from chronosynth import chronogram
-
+import json
 # chronogram.set_dev()
 # chronogram.set_prod()
 
+## We don't wnat to run these repeatedly...
+#sources = chronogram.find_trees()
+#resp = chronogram.combine_ages_from_sources(sources, json_out = "node_ages.json", failed_sources='no_conf.txt')
 
-sources = chronogram.find_trees()
-resp = chronogram.combine_ages_from_sources(sources, json_out = "node_ages.json", failed_sources='no_conf.txt')
 
+resp = json.load("node_ages.json")
