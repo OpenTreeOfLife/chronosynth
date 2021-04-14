7a25de8abd8febe8ab46738f5abd29e5dfd0b690
snacktavish <ejmctavish@gmail.com>
2021-04-14 15:09:44 -0700

    clean up output

--- examples/find_trees.py
+++ examples/find_trees.py
@@ -5,19 +5,19 @@ from chronosynth import chronogram
 studies = chronogram.find_trees()
 prod = set(studies)
 
-chronogram.print_endpoint()
+#chronogram.print_endpoint()
 
-chronogram.set_dev()
+#chronogram.set_dev()
 
-chronogram.print_endpoint()
+#chronogram.print_endpoint()
 
 
-studies = chronogram.find_trees()
-dev = set(studies)
+#studies = chronogram.find_trees()
+#dev = set(studies)
 
 print("{} chrongrams in production\n".format(len(prod)))
-print("{} chrongrams in dev\n".format(len(dev)))
+#print("{} chrongrams in dev\n".format(len(dev)))
 
-print(prod.difference(dev))
+#print(prod.difference(dev))
 
-print(dev.difference(prod))
+#print(dev.difference(prod))
