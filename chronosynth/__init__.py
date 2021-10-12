a4c16d3c21f11a2c4417d30f260b189bcedb53a2
snacktavish <ejmctavish@gmail.com>
2021-10-12 11:41:03 -0700

    logging config

--- chronosynth/__init__.py
+++ chronosynth/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 __version__ = "0.0.1"  # sync with setup.py
 
-import logging
+import logging.config
 import sys
+import os
 
-logging.basicConfig(filename='chronosynth.log', 
-                    filemode='w', 
-                    level=logging.DEBUG,
-                    format='%(name)s - %(levelname)s - %(message)s')
+#https://medium.com/@narengowda/better-pyramid-logging-with-python-and-log-configurationsyoure-up-and-running-c47203dfaff2
 
+configfile = chronosynth_dir = os.path.dirname(__file__) + '/../log.config'
+logging.config.fileConfig(configfile)
